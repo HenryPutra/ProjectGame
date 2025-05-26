8fee082a801529650353c598e6e6d3572dd42888
Hendri Restu Eko Saputra <117793932+HenryPutra@users.noreply.github.com>
2025-05-26 14:28:47 +0700

    second commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,54 +1,165 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-   [SerializeField] private float speed;
-   [SerializeField] private LayerMask groundLayer;
-   private Rigidbody2D body;
-   private Animator anim;
-   private BoxCollider2D boxCollider;
+    [Header("Player Movement")]
+    public float moveSpeed = 5f;
+    public float runSpeed = 8f;
+    public float jumpForce = 10f;
 
-   private void Awake()
-   {
-        body = GetComponent<Rigidbody2D>();
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private PlayerController playerController; // tambahkan PlayerInputActions
+
+    // Untuk input dari button UI
+    private float mobileInputX = 0f;
+
+    private Vector2 moveInput;
+    private bool isJumping = false;
+
+    private enum MovementState { idle, walk, jump, fall, run}
+
+    [Header("Jump Settings")]
+    [SerializeField] private LayerMask jumpableGround;
+    private BoxCollider2D coll;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        boxCollider = GetComponent<BoxCollider2D>();
-   }
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
 
-   private void Update()
-    {   
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed,body.velocity.y);
+        playerController = new PlayerController(); //Inisialisasi PlayerInputActions
+    }
 
-        if (horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
+    private void OnEnable()
+    {
+        playerController.Enable();
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded())
-            Jump();
+        playerController.Movement.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        playerController.Movement.Move.canceled += ctx => moveInput = Vector2.zero;
 
-        anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", isGrounded());
+        playerController.Movement.Jump.performed += ctx => Jump();
+
+        
+    }
+
+    private void OnDisable()
+    {
+        playerController.Disable();
+    }
+
+    private void Update()
+    {
+        // Jika menggunakan mobile input, pakai itu
+        if (Application.isMobilePlatform)
+        {
+            moveInput = new Vector2(mobileInputX, 0f);
+        }
+        else
+        {
+            // Kalau bukan mobile, pakai Input System
+            moveInput = playerController.Movement.Move.ReadValue<Vector2>();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //gabungan mobile
+        Vector2 targetVelocity = new Vector2((moveInput.x + mobileInputX) * moveSpeed, rb.velocity.y);
+        rb.velocity = targetVelocity;
+
+        UpdateAnimation();
+
+        // Reset isJumping hanya saat grounded dan velocity Y mendekati 0
+        if (isGrounded() && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            isJumping = false;
+        }
+
+    }
+
+    private void UpdateAnimation()
+    {
+        MovementState state;
+
+        // Gabungkan input dari keyboard dan mobile
+        float horizontal = moveInput.x != 0 ? moveInput.x : mobileInputX;
+
+        // Cek arah jalan
+        if (horizontal > 0f)
+        {
+            state = MovementState.walk;
+            sprite.flipX = false;
+        }
+        else if (horizontal < 0f)
+        {
+            state = MovementState.walk;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        // Cek apakah sedang lompat atau jatuh
+        if (rb.velocity.y > 0.1f)
+        {
+            state = MovementState.jump;
+        }
+        else if (rb.velocity.y < -0.1f)
+        {
+            state = MovementState.fall;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("jump");
+        // Cek ulang grounded saat ini, dan jangan gunakan isJumping (karena bisa delay)
+        if (isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    // Fungsi ini dipanggil saat tombol kanan ditekan
+    public void MoveRight(bool isPressed)
     {
-        
+        if (isPressed)
+            mobileInputX = 1f;
+        else if (mobileInputX == 1f)
+            mobileInputX = 0f;
     }
 
-    private bool isGrounded()
+    public void MoveLeft(bool isPressed)
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        if (isPressed)
+            mobileInputX = -1f;
+        else if (mobileInputX == -1f)
+            mobileInputX = 0f;
     }
-   
+
+    // Fungsi ini dipanggil saat tombol lompat ditekan
+    public void MobileJump()
+    {
+        if (isGrounded())
+        {
+            Jump();
+        }
+    }
 }
